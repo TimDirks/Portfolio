d7dff9f7d28a7684529b1006f04057a129e47298
Tim Dirks <tim.dirks@hotmail.com>
2023-08-21 17:25:18 +0200

    Start on random pop up sprite

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,12 @@
         <MagicCursor class="z-50" />
 
         <RouterView />
+
+        <PopUpSprite />
     </div>
 </template>
 
 <script lang="ts" setup>
 import MagicCursor from '@/components/common/MagicCursor.vue';
+import PopUpSprite from '@/components/common/PopUpSprite.vue';
 </script>
---
+++ src/components/common/PopUpSprite.vue
@@ -0,0 +1,23 @@
+<template>
+    <div
+        class="fixed"
+        :style="randomPosition"
+    >
+        <img
+            class="h-24"
+            src="@/assets/img/sprite-tim.png"
+        >
+    </div>
+</template>
+
+<script lang="ts" setup>
+import {onMounted, ref} from 'vue';
+
+const randomPosition = ref<string>('');
+
+const generateNewPosition = () => {
+    randomPosition.value = 'top: 50%; right: 50%';
+};
+
+onMounted(generateNewPosition);
+</script>
