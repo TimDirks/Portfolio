96a39e5f273bcf90c34ff5bf97e5bfb3bdcf8c35
Tim Dirks <tim.dirks@hotmail.com>
2023-08-23 17:37:23 +0200

    Work on portrait images

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -9,5 +9,5 @@
 </template>
 
 <script lang="ts" setup>
-import Footer from '@/components/common/Footer.vue';
+import Footer from '@/components/Footer.vue';
 </script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
-        <Intro class="mb-4" />
+    <div class="grid grid-cols-2">
+        <div>
+            <Intro class="mb-4" />
 
-        <Contact />
+            <Contact />
+        </div>
+
+        <Portrait />
     </div>
 </template>
 
 <script lang="ts" setup>
 import Contact from '@/components/Contact.vue';
 import Intro from '@/components/Intro.vue';
+import Portrait from '@/components/common/Portrait.vue';
 </script>
