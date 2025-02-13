0c454b6c77e392befed51d9b8c517887b25998f1
Tim Dirks <t.dirks@nobears.nl>
2025-02-13 12:45:52 +0100

    TD-4: Add flower positions and max flower count

---
+++ src/components/garden/GardenLawn.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+    <div class="garden-lawn h-60 max-w-4xl bg-lime-400">
+        <slot />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.garden-lawn {
+    border-radius: 50% / 50% 50% 0 0;
+}
+</style>
